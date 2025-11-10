0ad32dfeb22fbac36c9d02536dc319d9e4cf5f0c
Khalid Altamimi <khalida.altamimi71@gmail.com>
2025-11-10 18:39:55 +0300

    Dashboard and Landing page removed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import NavBar from './components/NavBar/NavBar';
 // Import the SignUpForm component
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
-import Landing from './components/Landing/Landing';
-import Dashboard from './components/Dashboard/Dashboard';
+//import Landing from './components/Landing/Landing';
+//import Dashboard from './components/Dashboard/Dashboard';
 import { useContext } from 'react';
 import { UserContext } from './contexts/UserContext';
 
