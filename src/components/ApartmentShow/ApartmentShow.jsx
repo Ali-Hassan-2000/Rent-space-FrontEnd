4c4194c4b349da3b2025c481c89178d0fb538cf1
Khalid Altamimi <khalida.altamimi71@gmail.com>
2025-11-10 20:10:24 +0300

    Creating appartment price in apartmentShow.jsx

--- src/components/ApartmentShow/ApartmentShow.jsx
+++ src/components/ApartmentShow/ApartmentShow.jsx
@@ -27,8 +27,53 @@ const ApartmentShow = () => {
             return;
         }
     };
-        
 
-}
+    const handleBookNow = () => {
+        if (!user) {
+            navigate (`/sign-in?next=/apartments${id}`);
+            return
+        }
+    };
+
+    return (
+        <main>
+            <section>
+                <h1>{apt.ApartmentName}</h1>
+                <button onClick={handleFavorite}>Favorite</button>
+            </section>
+
+            <section>
+                <h2>Apartment location</h2>
+                <p>{apt.ApartmentCity}</p>
+            </section>
+
+            <section>
+                <h2>Apartment offers</h2>
+                <p>{apt.ApartmentOffering}</p>
+            </section>
+
+            <aside>
+                <div>
+                    <label>starting date</label><br />
+                    <input
+                    type="data"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <h3>Apartment Price (per day)</h3>
+                    <p>{apt.ApartmentPrice}</p>
+                </div>
+
+                <button onClick={handleBookNow}>Book now</button>
+            </aside>
+        </main>
+    );
+
+};
+
+export default ApartmentShow;
 
 
