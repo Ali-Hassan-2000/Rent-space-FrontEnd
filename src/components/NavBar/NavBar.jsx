f9b032f4ad63f1857105d8a1e85d8d9f120cd483
Khalid Altamimi <khalida.altamimi71@gmail.com>
2025-11-10 18:51:49 +0300

    Navbar updated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 // Import the useContext hook
 import { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router';
 
 // Import the UserContext object
 import { UserContext } from '../../contexts/UserContext';
