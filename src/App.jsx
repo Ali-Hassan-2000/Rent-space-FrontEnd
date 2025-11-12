aaec8099a41f62298901b916860ea3ea4f915ff5
ali <ali.hassan.salman.2000@gmail.com>
2025-11-12 19:46:50 +0300

    add apartment form route

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './components/Home/Home';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Footer from './components/Footer/Footer';
+import AddApartmentForm from './components/AddApartment/AddApartmnetForm';
 
 const App = () => {
   const { user } = useContext(UserContext);
@@ -19,6 +20,7 @@ const App = () => {
 
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
+        <Route path='/apartments/new' element={<AddApartmentForm />} />
         
         <Route path= '*' element={<Navigate to = '/' replace />} />
       </Routes>
