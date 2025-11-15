129de9756c188551815a1bd68da7dca034b0abc4
ahq_292 <ali.quraishy2014@email.com>
2025-11-15 15:31:11 +0300

    adding use context

--- src/components/userBookings/userBookings.jsx
+++ src/components/userBookings/userBookings.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 const userBookings = ({userId}) => {
+  const { user } = useContext(UserContext);
   const [bookings,setBookings] = useState([])
 const [loading,setLoading] = useState(false)
 const [err,setErr] = useState('')
